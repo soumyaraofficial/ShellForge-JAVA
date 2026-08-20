9992f798b2a7f93ec9b296990da290b14cbcecd2
soumya ranjan panda <soumyaranjanpanda@soumyas-MacBook-Air.local>
2026-08-21 00:46:28 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
         while(true){
         System.out.print("$ ");
         String command = sc.nextLine();
+        if(command.equals("exit"))break;
         System.out.println(command +": command not found");}
     }
 }
