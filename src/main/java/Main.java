2f0bb3156ba9912f57270e85aebc956e729673d2
soumya ranjan panda <soumyaranjanpanda@soumyas-MacBook-Air.local>
2026-08-21 00:39:37 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-        System.out.print("$ ");
         Scanner sc = new Scanner(System.in);
+        while(true){
+        System.out.print("$ ");
         String command = sc.nextLine();
-        System.out.print(command +": command not found");
+        System.out.print(command +": command not found");}
     }
 }
