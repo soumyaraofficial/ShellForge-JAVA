a0327099b17bdf8ed882dd71337aa97012786849
soumya ranjan panda <soumyaranjanpanda@soumyas-MacBook-Air.local>
2026-08-21 20:28:53 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,7 +22,7 @@ public class Main {
                         System.out.println(type +" is a shell builtin");
                         break;
                     default:
-                        System.out.println(type +" : command not found");
+                        System.out.println(type +" : not found");
                         break;
                 }
                // System.out.println(type +" is a shell builtin");
