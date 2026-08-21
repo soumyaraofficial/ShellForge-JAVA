e4bebe51bbf8ad6099e9b02bd1d791cb72ad386f
soumya ranjan panda <soumyaranjanpanda@soumyas-MacBook-Air.local>
2026-08-21 20:26:35 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,21 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         Scanner sc = new Scanner(System.in);
-        while(true){
-        System.out.print("$ ");
-        String command = sc.nextLine();
-        if(command.equals("exit")){
-            break;
-        }else if(command.substring(0,4).equals("echo")){
-            System.out.println(command.substring(5,command.length()));
-        }else{
-        System.out.println(command +": command not found");}}
+        while (true) {
+            System.out.print("$ ");
+            String command = sc.nextLine();
+            if (command.equals("exit")) {
+                break;
+            } else if (command.substring(0, 4).equals("echo")) {
+                System.out.println(command.substring(5, command.length()));
+            } else if (command.substring(0, 4).equals("type")){
+               
+                String type = command.substring(5, command.length());
+                System.out.println(type +" is a shell builtin");
+
+            } else {
+                System.out.println(command + ": command not found");
+            }
+        }
     }
 }
