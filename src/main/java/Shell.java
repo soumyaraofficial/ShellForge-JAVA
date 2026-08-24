36a86c8df3e162b65adf354842c93f507c14f3f0
soumya ranjan panda <soumyaranjanpanda@soumyas-MacBook-Air.local>
2026-08-24 21:54:11 +0530

    codecrafters submit [skip ci]

--- src/main/java/Shell.java
+++ src/main/java/Shell.java
@@ -1,29 +1,47 @@
 import java.nio.file.Path;
-import java.util.Scanner;
+
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.impl.completer.StringsCompleter;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 
 public class Shell {
 
-    private final Scanner scanner = new Scanner(System.in);
     private final CommandExecutor executor = new CommandExecutor();
-
-
 
     private Path currentDirectory = Path.of("").toAbsolutePath();
 
     public void run() throws Exception {
 
+        Terminal terminal = TerminalBuilder.builder()
+                .system(true)
+                .build();
+        StringsCompleter completer = new StringsCompleter(
+                "echo",
+                "cd",
+                "pwd",
+                "exit");
+
+        LineReader reader = LineReaderBuilder.builder()
+                .terminal(terminal)
+                .completer(completer)
+                .build();
         while (true) {
 
-            System.out.print("$ ");
-
-            String command = scanner.nextLine();
+            String command = reader.readLine("$ ");
 
             if (command.isBlank()) {
                 continue;
             }
-            if(command.equalsIgnoreCase("EXIT"))break;
 
-        currentDirectory =  executor.execute(command, currentDirectory);
+            if (command.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            currentDirectory = executor.execute(command, currentDirectory);
         }
+
+        terminal.close();
     }
 }
